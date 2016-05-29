66e4a2bca265966905a096452f4e1aa856bccf4f
Silviu <novitchi.s@gmail.com>
2016-05-29 15:43:55 +0300

    Fixed Stockfish ARM build.

--- Stockfish/Stockfish/pch.h
+++ Stockfish/Stockfish/pch.h
@@ -2,6 +2,10 @@
 
 #define _CRT_SECURE_NO_WARNINGS 1
 
+// this is to fix the arm build
+// don't know yet what disabling pre-fetching means
+#define NO_PREFETCH 1
+
 #include <collection.h>
 #include <ppltasks.h>
 
