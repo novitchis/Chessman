e315af5113d210bd40da1f207709040f2e2aeffe
Silviu <novitchi.s@gmail.com>
2016-05-30 22:18:46 +0300

    Added new C++/CX project for UCIEngine implementation.

--- ChessEngineClient/Stockfish/StockfishComponent.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "StockfishComponent.h"
-
-
-Stockfish::StockfishComponent::StockfishComponent()
-{
-}
---
+++ ChessEngineClient/UciEngineComponent/Stockfish.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "Stockfish.h"
+
+using namespace UciEngineComponent;
+using namespace Platform;
+
+Stockfish::Stockfish()
+{
+}
