773d706050740eb16d63db39ea4cfb3983fb65e9
Silviu Novitchi <silviu.novitchi@cti.pub.ro>
2017-01-15 14:07:32 +0200

    Some engine strength adjustments.

--- ChessEngineClient/ChessEngineClient/Pages/ComputerPractice/PracticeBoardService.cs
+++ ChessEngineClient/ChessEngineClient/Pages/ComputerPractice/PracticeBoardService.cs
@@ -102,10 +102,19 @@ namespace ChessEngineClient
 
         public void SetEngineStrength(int strengthValue)
         {
-            // skill level : 0..20
-            engine.SetOptions(new EngineOptions() { SkillLevel = strengthValue * 2});
+            // reajust the engine skill level since the level 2 is already too strong
+            // strength(1..10) - skill level(0..20)
+            int skillLevel = 0;
+            if (strengthValue <= 3)
+                skillLevel = strengthValue - 1;
+            else if (strengthValue < 6)
+                skillLevel = (strengthValue - 1) * 2;
+            else
+                skillLevel = strengthValue * 2;
+
+            engine.SetOptions(new EngineOptions() { SkillLevel = skillLevel });
             // this can be further adjusted
-            secondsLeft = strengthValue * 20;
+            secondsLeft = skillLevel;
         }
     }
 }
