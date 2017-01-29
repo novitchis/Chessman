32624a38ccde517704a45346263f648f04aaee8e
Silviu Novitchi <silviu.novitchi@cti.pub.ro>
2017-01-29 19:41:18 +0200

    Removed warning.

--- ChessEngineClient/ChessEngineClient/App.xaml.cs
+++ ChessEngineClient/ChessEngineClient/App.xaml.cs
@@ -31,8 +31,6 @@ namespace ChessEngineClient
     /// </summary>
     sealed partial class App : Application
     {
-        private MessageDialog exitConfirmationDialog = null;
-
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
