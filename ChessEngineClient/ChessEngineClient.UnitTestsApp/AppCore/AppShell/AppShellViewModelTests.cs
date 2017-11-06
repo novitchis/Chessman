1c9aee606483d9baea837c4449824b8ff031b019
Silviu Novitchi <silviu.novitchi@cti.pub.ro>
2017-11-06 23:58:13 +0200

    Add unit test for AppShellViewModel.

--- ChessEngineClient/ChessEngineClient.UnitTestsApp/AppCore/AppShell/AppShellViewModelTests.cs
+++ ChessEngineClient/ChessEngineClient.UnitTestsApp/AppCore/AppShell/AppShellViewModelTests.cs
@@ -1,57 +1,84 @@
-﻿using System;
+﻿using ChessEngineClient;
+using Framework.MVVM;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace ChessEngineClient.UnitTests
+public class AppShellViewModelTests
 {
-    public class AppShellViewModelTests
+    private static string TestPageName = "TestPageName";
+    private static string TestSecondaryPageName = "TestSecondaryPageName";
+
+    private AppShellViewModel GetViewModel(INavigationService fakeNavigationService)
     {
-        [Fact]
-        public void AppShellViewModel_CurrentPageChanged_CallsOnCurrentPageChanged()
-        {
-        }
+        AppShellViewModel vm = new AppShellViewModel(fakeNavigationService);
+        vm.NavigationItems.Insert(0, new NavMenuItem(null, null, TestPageName));
+        vm.SecondaryNavigationItems.Insert(0, new NavMenuItem(null, null, TestSecondaryPageName));
 
-        [Fact]
-        public void OnCurrentPageChanged_OnNavItemKey_SetsCurrentNavItem()
-        {
-        }
+        return vm;
+    }
 
-        [Fact]
-        public void OnCurrentPageChanged_OnNavItemKey_NullsCurrentSecondaryNavItem()
-        {
-        }
+    [Fact]
+    public void CurrentPageChanged_OnNavItemKey_SetsCurrentNavItem()
+    {
+        Mock<INavigationService> mockNavigationService = new Mock<INavigationService>();
+        AppShellViewModel vm = GetViewModel(mockNavigationService.Object);
 
-        [Fact]
-        public void OnCurrentPageChanged_OnSecondaryNavItemKey_SetsCurrentSecondaryNavItem()
-        {
-        }
+        mockNavigationService.Setup(service => service.CurrentPageKey).Returns(TestPageName);
+        mockNavigationService.Raise(service => service.CurrentPageChanged += null, EventArgs.Empty);
 
-        [Fact]
-        public void OnCurrentPageChanged_OnNavItemKey_NullsCurrnetNavItem()
-        {
-        }
+        Assert.Equal(vm.CurrentNavItem.PageNavigationName, TestPageName);
+        Assert.Null(vm.CurrentSecondaryNavItem);
+    }
 
-        [Fact]
-        public void CurrentNavItem_WhenSet_CallsOnCurrentNavItemChanged()
-        {
-        }
+    [Fact]
+    public void CurrentPageChanged_WhenRaised_DoesntCallNavigateTo()
+    {
+        Mock<INavigationService> fakeNavigationService = new Mock<INavigationService>();
+        AppShellViewModel vm = GetViewModel(fakeNavigationService.Object);
 
-        [Fact]
-        public void CurrentSecondaryNavItem_WhenSet_CallsOnCurrentNavItemChanged()
-        {
-        }
+        fakeNavigationService.Setup(service => service.CurrentPageKey).Returns(TestPageName);
+        fakeNavigationService.Raise(service => service.CurrentPageChanged += null, EventArgs.Empty);
 
-        [Fact]
-        public void OnCurrentNavItemChanged_OnDifferentPageKey_CallsNavigateTo()
-        {
-        }
+        fakeNavigationService.Verify(service => service.NavigateTo(TestPageName), Times.Never());
+    }
 
-        [Fact]
-        public void OnCurrentNavItemChanged_OnSamePageKey_DoesntCallNavigateTo()
-        {
-        }
+    [Fact]
+    public void CurrentPageChanged_OnSecondaryNavItemKey_SetsCurrentSecondaryNavItem()
+    {
+        Mock<INavigationService> mockNavigationService = new Mock<INavigationService>();
+        AppShellViewModel vm = GetViewModel(mockNavigationService.Object);
+
+        mockNavigationService.Setup(service => service.CurrentPageKey).Returns(TestSecondaryPageName);
+        mockNavigationService.Raise(service => service.CurrentPageChanged += null, EventArgs.Empty);
+
+        Assert.Equal(vm.CurrentSecondaryNavItem.PageNavigationName, TestSecondaryPageName);
+        Assert.Null(vm.CurrentNavItem);
+    }
+
+    [Fact]
+    public void CurrentNavItem_WhenSet_CallsNavigateTo()
+    {
+        Mock<INavigationService> fakeNavigationService = new Mock<INavigationService>();
+        AppShellViewModel vm = GetViewModel(fakeNavigationService.Object);
+
+        vm.CurrentNavItem = vm.NavigationItems[0];
+        
+        fakeNavigationService.Verify(service => service.NavigateTo(vm.NavigationItems[0].PageNavigationName));
+    }
+
+    [Fact]
+    public void CurrentSecondaryNavItem_WhenSet_CallsNavigateTo()
+    {
+        Mock<INavigationService> fakeNavigationService = new Mock<INavigationService>();
+        AppShellViewModel vm = GetViewModel(fakeNavigationService.Object);
+
+        vm.CurrentSecondaryNavItem = vm.SecondaryNavigationItems[0];
+
+        fakeNavigationService.Verify(service => service.NavigateTo(vm.SecondaryNavigationItems[0].PageNavigationName));
     }
 }
